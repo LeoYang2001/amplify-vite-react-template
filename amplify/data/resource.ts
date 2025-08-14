7f0bffd3a69cc215cad106c09640b8c1f653c07d
LeoYang2001 <jya261@uky.edu>
2025-08-14 14:33:46 -0400

    schema error fix

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,5 +1,24 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
-import { Word } from "../types/Word";
+
+const Phonetic = a.model({
+  text: a.string(),
+  audioUrl: a.string(),
+});
+const Meaning = a.model({
+  definition: a.string(),
+  partOfSpeech: a.string(),
+  synonyms: a.string().array(),
+  antonyms: a.string().array(),
+});
+
+const Word = a.model({
+  id: a.string(),
+  word: a.string(),
+  imgUrl: a.string(),
+  meanings: a.ref("Meaning").array(),
+  phonetics: a.ref("Phonetic").array(),
+  timeStamp: a.string(),
+});
 
 const schema = a
   .schema({
